c1f0fec29a55fdc4e32e3c2ec362f44b668d495c
nacen.dev <nacen.dev@gmail.com>
2022-03-25 23:02:03 +0800

    Add tabSwitch logic and modify styling of nav

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -9,18 +9,45 @@ const Nav = () => {
 
   const menu = document.createElement("li");
   menu.textContent = "Menu";
+  menu.dataset.id = "menu";
+  menu.classList.add("nav-list-item");
   const contact = document.createElement("li");
   contact.textContent = "Contact";
-  const aboutUs = document.createElement("li");
-  aboutUs.textContent = "About Us";
+  contact.dataset.id = "contact";
+  contact.classList.add("nav-list-item");
+  const about = document.createElement("li");
+  about.textContent = "About Us";
+  about.dataset.id = "about"
+  about.classList.add("nav-list-item");
 
   navList.appendChild(menu);
   navList.appendChild(contact);
-  navList.appendChild(aboutUs);
+  navList.appendChild(about);
 
   nav.appendChild(navList);
 
   return nav;
 };
 
-export { Nav };
+const navTabSwitch = () => {
+  const navItems = document.querySelectorAll(".nav-list-item");
+
+  navItems.forEach((navItem) => {
+    navItem.addEventListener("click", () => {
+      const content = document.querySelector(".content");
+      
+      if (content.hasChildNodes()) {
+        let childNodes = content.childNodes;
+        for (let i = 0; i < childNodes.length; i++) {
+          if (childNodes[i].dataset.id !== navItem.dataset.id) {
+            childNodes[i].classList.add("hidden"); 
+          } else {
+            childNodes[i].classList.remove("hidden");
+          }
+        }
+      }
+    });
+  });
+};
+
+export { Nav, navTabSwitch };
