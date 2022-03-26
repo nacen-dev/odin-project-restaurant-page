7bc549c066ba70d41dcce308f2618cd5766ffe93
nacen.dev <nacen.dev@gmail.com>
2022-03-26 21:01:20 +0800

    Create Content component

---
+++ src/components/content/content.js
@@ -0,0 +1,20 @@
+import { About } from "../about/about";
+import { Contact } from "../contact/contact";
+import { Menu } from "../menu/menu";
+
+import "./content.scss";
+
+const Content = () => {
+  const content = document.createElement("div");
+  content.classList.add('content');
+  content.appendChild(Menu())
+  const contact = Contact();
+  contact.classList.add("hidden");
+  content.appendChild(contact)
+  const about = About()
+  contact.classList.add("hidden");
+  content.appendChild(about)
+  return content;
+};
+
+export { Content };
