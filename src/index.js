02a01eb5fffc300d062b8f4c100a8a7c16e0b2d5
nacen.dev <nacen.dev@gmail.com>
2022-03-26 21:02:33 +0800

    Add Container to index and run navTabSwitch

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import "./style.scss";
-import { Nav } from "./components/nav/nav";
+import { Nav, navTabSwitch } from "./components/nav/nav";
+import { Container } from "./components/container/container";
+
 
 document.body.appendChild(Nav());
+document.body.appendChild(Container());
+
+navTabSwitch();
